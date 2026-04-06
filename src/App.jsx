8efbae8e91c98041a226554ef9dd46aab7fa4b8a
jahidsharuar2021-prog <jahidsharuar2021@gmail.com>
2026-04-06 16:42:46 +0600

    DigiTools Platform

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Footer from "./Components/Footer/Footer";
+import Navbar from "./Components/Navbar & Banner/Navbar";
+
+
+function App() {
+
+
+  return (
+    <>
+     <Navbar/>
+    <Footer/>
+    
+    </>
+  );
+}
+
+export default App
