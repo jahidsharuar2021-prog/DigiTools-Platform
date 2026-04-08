ae91447c644e18c4f78a779fabc24103a2decd90
jahidsharuar2021-prog <jahidsharuar2021@gmail.com>
2026-04-08 16:58:17 +0600

    card designed

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,24 @@ import Footer from "./Components/Footer/Footer";
 import Hero from "./Components/NavbarAndBanner/Hero";
 import Navbar from "./Components/NavbarAndBanner/Navbar";
 import Rating from "./Components/NavbarAndBanner/Rating";
+import Cards from "./Components/premiumTools/Cards";
+import Degitools from "./Components/premiumTools/Degitools";
 import WorkSection from "./Components/Your Work/WorkSection";
+
+
 
 
 function App() {
 
-
   return (
     <>
-     <Navbar/>
-     <Hero/>
-     <Rating/>
-     <WorkSection/>
-    <Footer/>
+      <Navbar />
+      <Hero />
+      <Rating />
+      <Degitools />
     
+      <WorkSection />
+      <Footer />
     </>
   );
 }
---
+++ src/Components/premiumTools/Degitools.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Degitools = () => {
+    return (
+      <div className="hero bg-base-200 min-h-49 md:pt-[100px]">
+        <div className="hero-content text-center">
+          <div className="">
+            <h1 className="text-5xl font-bold">Premium Digital Tools</h1>
+            <p className="py-6 text-[#627382]">
+              Choose from our curated collection of premium digital products
+              designed <br /> to boost your productivity and creativity.
+            </p>
+            <div className="flex gap-4 justify-center ">
+              <button className="liner-button btn text-white ">Products</button>
+              <button className="  btn btn-ghost">Cart (2)</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+};
+
+export default Degitools;
